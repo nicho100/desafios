5188fb8d4c347b2b7557891c789f670bdf6a187d
nicho100 <63875333+nicho100@users.noreply.github.com>
2022-11-23 17:12:33 -0300

    Update segundo_desafio.js

--- segundo_desafio.js
+++ segundo_desafio.js
@@ -1,40 +1,34 @@
-
-//const { info } = require('console')
 const fs=require('fs')
 const file='./archivos/productos.txt'
+
 try{let file ='./archivos/productos.txt'
 fs.mkdirSync('./archivos')
-fs.writeFileSync(file,'hola')
 }catch(err){
 console.error(err)
 }
 
 
-
 class contenedor{
-    constructor(title,price,thumbnail){
-        this.title=title,
-        this.price=price
-        this.thumbnail=thumbnail
-         
+    constructor(archivo){
+        this.archivo=archivo
+        
     }
     save(object){
         //traer el contenido del archivo y preguntar si tiene algo,si no se pone objet id en 1
         //si hay contenido se recorre y se guarda el id del ultimo y se le suma uno y al objeto.id se le asigna lo guardado
-        fs.readFileSync(file,'utf-8',(error,contenido)=>{
+        fs.readFileSync(this.archivo,'utf-8',(error,contenido)=>{
             if (error){
                 console.log("no se pudo leer el archivo")
             }else{
-                console.log("hola")
+                
                 let info =JSON.parse(contenido)
                 if (info ==undefined){  
                     object["id"]=1
                    
-                    fs.writeFileSync('./archivos/.txt',object)
+                    fs.writeFileSync(this.archivo,object)
                     return(object.id)
                 }else{
                     let info=JSON.parse(contenido)
-                    console.log("hola2")
                     mayor=-1
                     for(let i = 0;i <info.length;i++){
                     if (info[i].id>mayor){
@@ -42,7 +36,7 @@ class contenedor{
                     }}
                     object["id"]=mayor+1
                     info.push(object)
-                    fs.writeFileSync('./archivos/producos.txt',JSON.stringify(info,null,2))
+                    fs.writeFileSync(this.archivo,JSON.stringify(info,null,2))
                     return(mayor+1)
                  
                 }
@@ -53,7 +47,7 @@ class contenedor{
     }
     getbyid(number){
         
-        fs.readFileSync(file,'utf-8',(error,contenido)=>{
+        fs.readFileSync(this.archivo,'utf-8',(error,contenido)=>{
             if (error){
                 console.log("no se pudo leer el archivo")
             }else{
@@ -72,10 +66,60 @@ class contenedor{
                 }
             })
     }
+    getAll(){
+        let info=""
+        fs.readFileSync(this.archivo,'utf-8',(error,contenido)=>{
+            if (error){
+                console.log("no se pudo leer el archivo")
+            }else{
+                console.log("hola")
+                info =JSON.parse(contenido)
+                
+            }
+            
+        })
+        return info
+    }
+    deleteById(number){
+        let info=""
+        fs.readFileSync(this.archivo,'utf-8',(error,contenido)=>{
+            if (error){
+                console.log("no se pudo leer el archivo")
+            }else{
+                console.log("hola")
+                info =JSON.parse(contenido)
+                for(let i = 0;i <info.length;i++){
+                    if (info[i].id===number){
+                        info.splice(i,1)
+                        fs.writeFileSync(this.archivo,JSON.stringify(info,null,2))
+                          }else{
+                            console.log("el elemento no se encuentra en el archivo")
+                          } 
+                        } 
+                
+                }
+           
+            
+        }) 
+    }
+    deleteAll(){
+        let info=""
+        fs.readFileSync(this.archivo,'utf-8',(error,contenido)=>{
+            if (error){
+                console.log("no se pudo leer el archivo")
+            }else{ 
+                info =JSON.parse(contenido)
+                info.splice(0,info.length)
+                fs.writeFileSync(this.archivo,JSON.stringify(info,null,2))
+            }
+        })
+    }
 }
 
-const producto=new contenedor("ventilador",300,"link");
-let obj={title:"ventilador",price:300,url:"link"}
+const producto=new contenedor(file);
+let object={title:"ventilador",price:300,url:"link"}
 console.log(producto.getbyid(2))
-console.log(producto.save(obj))
-//fs.appendFile('./archivos/ejemplo.txt',objeto)
+console.log(producto.save(object))
+console.log(producto.getAll())
+producto.deleteById(1)
+producto.deleteAll()
